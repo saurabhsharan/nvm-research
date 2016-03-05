865a536280f32543e57d184cdb9f4e1020e5bd73
Saurabh Sharan <saurabh1@stanford.edu>
2016-03-04 17:34:33 -0800

    Add pin support

--- parsec.py
+++ parsec.py
@@ -11,16 +11,30 @@ COMMAND_LINE_ARGS = {
 
 VALID_INPUT_SIZES = ["test", "simdev", "simsmall", "simmedium", "simlarge", "native"]
 
-def main(app_name, input_size):
+PIN_BASE_DIR_PATH = os.path.join(os.getcwd(), "pin")
+PIN_APP_PATH = os.path.join(PIN_BASE_DIR_PATH, "pin")
+PIN_TOOL_PATH = os.path.join(PIN_BASE_DIR_PATH, "sources/tools/ManualExamples/obj-intel64/pinatrace.so")
+
+PIN_COMMAND = " ".join([PIN_APP_PATH, "-t", PIN_TOOL_PATH, "--", "%s"])
+
+def full_parsec_command(app_name, input_size):
+  if app_name not in COMMAND_LINE_ARGS:
+    raise ValueError("Invalid app_name %s, must be one of: %s" % (app_name, ", ".join(COMMAND_LINE_ARGS.keys())))
+
+  if input_size not in VALID_INPUT_SIZES:
+    raise ValueError("Invalid input_size %s, must be one of: %s" % (input_size, " ".join(VALID_INPUT_SIZES)))
+
   app_dir = "parsec-3.0/pkgs/apps/%s/" % app_name
 
   with util.untar_file(app_dir + "inputs/input_%s.tar" % input_size) as input_filename:
     with util.create_tmp_file() as output_filename:
       full_path_to_app = os.path.join(os.getcwd(), app_dir, "inst/amd64-linux.gcc/bin", app_name)
       command_line_args = COMMAND_LINE_ARGS[app_name] % (input_filename, output_filename)
-      print "%s %s" % (full_path_to_app, command_line_args)
-      os.system("%s %s" % (full_path_to_app, command_line_args))
+      return full_path_to_app + " " + command_line_args
 
+def main(app_name, input_size):
+  parsec_command = full_parsec_command(app_name, input_size)
+  print PIN_COMMAND % parsec_command
       # TODO(saurabh): for some reason subprocess.call() doesn't work here
       # return_code = subprocess.call("%s %s" % (full_path_to_app, command_line_args))
       # print return_code
