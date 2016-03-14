dea5844c3cb895dbf89b4f49cb03093fe726266b
Saurabh Sharan <saurabh1@stanford.edu>
2016-03-13 17:05:24 -0700

    Add histograms both with and without cache simulator to same file

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ import matplotlib
 # http://stackoverflow.com/a/3054314
 matplotlib.use('Agg')
 
+import json
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import os
@@ -20,21 +21,39 @@ def main(app_name):
 
   datapoints = []
 
+  total_accesses_with_cache = {}
+  total_accesses_without_cache = {}
+
   with open(path_to_latest_output_file) as f:
-    for line in f:
-      if line.strip() == "## TOTAL":
-        break
+    pin_data = json.load(f)
 
-    for line in f:
-      datapoints.append(int(line.strip().split(": ")[1]))
+  for pageno, count in pin_data["read_with_cache"].iteritems():
+    total_accesses_with_cache[int(pageno)] = total_accesses_with_cache.get(int(pageno), 0) + int(count)
+  for pageno, count in pin_data["write_with_cache"].iteritems():
+    total_accesses_with_cache[int(pageno)] = total_accesses_with_cache.get(int(pageno), 0) + int(count)
 
-  plt.hist(datapoints)
-  plt.xlabel("# of reads/writes to one page")
-  plt.ylabel("# of pages")
-  plt.title(app_name)
+  for pageno, count in pin_data["read_without_cache"].iteritems():
+    total_accesses_without_cache[int(pageno)] = total_accesses_without_cache.get(int(pageno), 0) + int(count)
+  for pageno, count in pin_data["write_without_cache"].iteritems():
+    total_accesses_without_cache[int(pageno)] = total_accesses_without_cache.get(int(pageno), 0) + int(count)
+
+
+  fig = plt.figure()
+
+  axis1 = fig.add_subplot(211)
+  axis1.hist(total_accesses_with_cache.values())
+  # axis1.xlabel("# of reads/writes to one page")
+  # axis1.ylabel("# of pages")
+  # axis1.title(app_name + " (with cache)")
+
+  axis2 = fig.add_subplot(212)
+  axis2.hist(total_accesses_without_cache.values())
+  # axis2.xlabel("# of reads/writes to one page")
+  # axis2.ylabel("# of pages")
+  # axis2.title(app_name + " (without cache)")
 
   output_graph_image_path = os.path.join(os.path.dirname(path_to_latest_output_file), "page-access-histogram.png")
-  plt.savefig(output_graph_image_path)
+  fig.savefig(output_graph_image_path)
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
